c5ed6216d6eeacbdf0f2edc657eef9a8d265d417
Jeremias Pérez <jeremiasperezcontell@gmail.com>
2026-03-18 13:45:10 +0000

    new workflow

--- test/frontend/component_sample.test.jsx
+++ test/frontend/component_sample.test.jsx
@@ -3,21 +3,22 @@ import userEvent from "@testing-library/user-event"
 import { describe, it, expect, vi } from "vitest"
 import '@testing-library/jest-dom/vitest'
 
-import Card from "../../src/components/Tarea"
+import Tarea from "../../src/components/Tarea"
 
 describe("Componente tarea", () => {
 
   it("Se muestra el título de la tarea", () => {
     const tarea = { id: 1, prioridad: 1, categoria: 'testing', responsable: 'jere', nombre: "Crear test" }
     const click = () => {}
-    render(<Card infoTarea={tarea} gestionarClicEnTarea={click} />)
-    expect(screen.getByText("Crear test")).toBeInTheDocument()
+    render(<Tarea infoTarea={tarea} gestionarClicEnTarea={click} />)
+    const el = screen.getByText("Crear test")
+    expect(el).toBeInTheDocument()
   })
 
   it("Se llama a la función recibida al hacer clic en la tarea", async () => {
     const tarea = { id: 1, prioridad: 1, categoria: 'testing', responsable: 'jere', nombre: "Crear test" }
     const funcionClick = vi.fn()
-    render(<Card infoTarea={tarea} gestionarClicEnTarea={funcionClick} />)
+    render(<Tarea infoTarea={tarea} gestionarClicEnTarea={funcionClick} />)
     const el = screen.getByText("Crear test")
     await userEvent.click(el)
     expect(funcionClick).toHaveBeenCalled()
